e5dccac0b49b001a2a4da85e1c95de2ff57752a9
bws9000 <snyder.burt@gmail.com>
2021-06-28 12:59:50 -0400

    add cors

--- index.js
+++ index.js
@@ -16,6 +16,13 @@ var btcEth = async () => {
     });
 }
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.setHeader('Access-Control-Allow-Methods', 'GET');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.get("/btc-eth-price", async (req, res, next) => {
     if (!timerStarted) {
         setTimeout(async function () {
